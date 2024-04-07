7e5d91c7cbec1f6e668f2edf08a3d22a58d1bbfd
Bruno Medeiros <eobruno@gmail.com>
2024-04-07 20:13:17 -0300

    react-navigation configurada

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>teste 3</Text>
+      <Text>teste 3</Text>
+      <Text>teste 3</Text>
+      <Text>teste 3</Text>
       <StatusBar style="auto" />
     </View>
   );
